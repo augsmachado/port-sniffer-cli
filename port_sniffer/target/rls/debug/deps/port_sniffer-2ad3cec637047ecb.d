262cedc8dc79d28b6deef894b9ce16d2f305a1cf
Augs Machado <augustomachadoramos@gmail.com>
2021-05-01 17:05:04 -0300

    [port_sniffer] Initial commit

---
+++ port_sniffer/target/rls/debug/deps/port_sniffer-2ad3cec637047ecb.d
@@ -0,0 +1,5 @@
+/Users/augustomachado/github/rust_port_sniffer_cli/port_sniffer/target/rls/debug/deps/port_sniffer-2ad3cec637047ecb.rmeta: src/main.rs
+
+/Users/augustomachado/github/rust_port_sniffer_cli/port_sniffer/target/rls/debug/deps/port_sniffer-2ad3cec637047ecb.d: src/main.rs
+
+src/main.rs:
---
+++ port_sniffer/target/rls/debug/deps/port_sniffer-ffb883ecb4bf6687.d
@@ -0,0 +1,5 @@
+/Users/augustomachado/github/rust_port_sniffer_cli/port_sniffer/target/rls/debug/deps/port_sniffer-ffb883ecb4bf6687.rmeta: src/main.rs
+
+/Users/augustomachado/github/rust_port_sniffer_cli/port_sniffer/target/rls/debug/deps/port_sniffer-ffb883ecb4bf6687.d: src/main.rs
+
+src/main.rs:
