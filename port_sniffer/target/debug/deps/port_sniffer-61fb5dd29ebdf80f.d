a60a24e94b2c38abf705857a317da4c57c4c1c2f
Augs Machado <augustomachadoramos@gmail.com>
2021-05-08 23:15:35 -0300

    .

---
+++ port_sniffer/target/debug/deps/port_sniffer-61fb5dd29ebdf80f.d
@@ -0,0 +1,5 @@
+/Users/augustomachado/github/rust_port_sniffer_cli/port_sniffer/target/debug/deps/port_sniffer-61fb5dd29ebdf80f.rmeta: src/main.rs
+
+/Users/augustomachado/github/rust_port_sniffer_cli/port_sniffer/target/debug/deps/port_sniffer-61fb5dd29ebdf80f.d: src/main.rs
+
+src/main.rs:
---
+++ port_sniffer/target/debug/deps/port_sniffer-6215a31b969d7e77.d
@@ -0,0 +1,5 @@
+/Users/augustomachado/github/rust_port_sniffer_cli/port_sniffer/target/debug/deps/port_sniffer-6215a31b969d7e77.rmeta: src/main.rs
+
+/Users/augustomachado/github/rust_port_sniffer_cli/port_sniffer/target/debug/deps/port_sniffer-6215a31b969d7e77.d: src/main.rs
+
+src/main.rs:
